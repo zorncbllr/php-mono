51737280defa99f3e543c0414c4599f2fa6fbb16
Manuel Caballero <164306498+zorncbllr@users.noreply.github.com>
2024-11-20 12:32:56 -0800

    Update Docs.php

--- src/middlewares/Docs.php
+++ src/middlewares/Docs.php
@@ -8,7 +8,7 @@ class Docs extends Middleware
 	{
 		echo '<a
 		style="text-decoration: none; color: gray; border: 1px solid gray; padding: 0.4rem 1rem; position: absolute; top: .5rem; left: .5rem"
-		href="https://github.com/zorncbllr/Mono">View Documentation</a>';
+		href="https://github.com/zorncbllr/Mono/blob/main/README.md">View Documentation</a>';
 
 		return $next();
 	}
