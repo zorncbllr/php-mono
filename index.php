e92c778e28ae58273971f62f6896204a0644d031
Manuel <caballeromanuel203@gmail.com>
2024-11-20 16:07:05 -0800

    made some performance optimization in response methods

--- index.php
+++ index.php
@@ -6,12 +6,8 @@ session_start();
 
 $config = (require_once __DIR__ . '/src/config/config.php')['cors'];
 
-$origin = $config['origin'];
-$methods = implode(', ', $config['allowed_methods']);
-$headers = implode(', ', $config['allowed_headers']);
-
-header("Access-Control-Allow-Origin: $origin");
-header("Access-Control-Allow-Methods: $methods");
-header("Access-Control-Allow-Headers: $headers");
+header("Access-Control-Allow-Origin: " + $config['origin']);
+header("Access-Control-Allow-Methods: " + implode(', ', $config['allowed_methods']));
+header("Access-Control-Allow-Headers: " + implode(', ', $config['allowed_headers']));
 
 require_once __DIR__ . '/src/core/__init.php';
