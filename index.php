d85e059767845b3302241cd7dd61c59697da44ee
Manuel <caballeromanuel203@gmail.com>
2024-11-20 16:08:22 -0800

    made things shorter

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ session_start();
 
 $config = (require_once __DIR__ . '/src/config/config.php')['cors'];
 
-header("Access-Control-Allow-Origin: " + $config['origin']);
-header("Access-Control-Allow-Methods: " + implode(', ', $config['allowed_methods']));
-header("Access-Control-Allow-Headers: " + implode(', ', $config['allowed_headers']));
+header("Access-Control-Allow-Origin: " . $config['origin']);
+header("Access-Control-Allow-Methods: " . implode(', ', $config['allowed_methods']));
+header("Access-Control-Allow-Headers: " . implode(', ', $config['allowed_headers']));
 
 require_once __DIR__ . '/src/core/__init.php';
