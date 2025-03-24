6a3a400c8388f47049409cdabe210634185b3996
Manuel <caballeromanuel203@gmail.com>
2025-03-25 00:01:45 +0800

    redesigned welcome page and 404 page

---
+++ src/views/Welcome.view.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+	<meta charset='UTF-8'>
+	<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+	<title>Home</title>
+	<link rel="stylesheet" href="css/tailwind-build.css">
+</head>
+
+<body>
+
+	<section class="bg-white dark:bg-gray-900 ">
+		<div class="container flex items-center justify-center min-h-screen px-6 py-12 mx-auto">
+			<div class="w-full ">
+
+				<div class="flex items-center justify-center w-full px-6 py-8 lg:h-[32rem] lg:w-1/2">
+					<div class="max-w-xl">
+						<h2 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Get started with <span class="text-blue-600 dark:text-blue-400">Mono</span></h2>
+
+						<p class="mt-4 text-sm text-gray-500 dark:text-gray-400 lg:text-base">Build powerful, modular web applications with Mono's MVC architecture, object-oriented design, and robust CLI tools—all in a lightweight, high-performance package.</p>
+
+						<div class="flex flex-col mt-6 space-y-3 lg:space-y-0 lg:flex-row">
+							<a href="https://github.com/zorncbllr/php-mono/blob/main/README.md" class="block px-5 py-2 text-sm font-medium tracking-wider text-center text-white transition-colors duration-300 transform bg-gray-900 rounded-md hover:bg-gray-700">Get Started</a>
+							<a href="https://github.com/zorncbllr/php-mono" class="block px-5 py-2 text-sm font-medium tracking-wider text-center text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md lg:mx-4 hover:bg-gray-300">Learn More</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="grid w-full max-w-6xl grid-cols-1 gap-8 mx-auto mt-8 sm:grid-cols-2 lg:grid-cols-3">
+					<div class="p-6 rounded-lg bg-blue-50 dark:bg-gray-800">
+						<span class="text-gray-500 dark:text-gray-400">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+							</svg>
+						</span>
+
+						<h3 class="mt-6 font-medium text-gray-700 dark:text-gray-200 ">Documentation</h3>
+
+						<p class="mt-2 text-gray-500 dark:text-gray-400 ">Dive in to learn all about our product.</p>
+
+						<a href="https://github.com/zorncbllr/php-mono/blob/main/README.md" class="inline-flex items-center mt-4 text-sm text-blue-500 gap-x-2 dark:text-blue-400 hover:underline">
+							<span>Start learning</span>
+
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 rtl:rotate-180">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+							</svg>
+						</a>
+					</div>
+
+					<div class="p-6 rounded-lg bg-blue-50 dark:bg-gray-800">
+						<span class="text-gray-500 dark:text-gray-400">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+							</svg>
+						</span>
+
+						<h3 class="mt-6 font-medium text-gray-700 dark:text-gray-200 ">Our blog</h3>
+
+						<p class="mt-2 text-gray-500 dark:text-gray-400 ">Read the latest posts on our blog.</p>
+
+						<a href="#" class="inline-flex items-center mt-4 text-sm text-blue-500 gap-x-2 dark:text-blue-400 hover:underline">
+							<span>View lastest posts</span>
+
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 rtl:rotate-180">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+							</svg>
+						</a>
+					</div>
+
+					<div class="p-6 rounded-lg bg-blue-50 dark:bg-gray-800">
+						<span class="text-gray-500 dark:text-gray-400">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 01-.923 1.785A5.969 5.969 0 006 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337z" />
+							</svg>
+						</span>
+
+						<h3 class="mt-6 font-medium text-gray-700 dark:text-gray-200 ">Chat to us</h3>
+
+						<p class="mt-2 text-gray-500 dark:text-gray-400 ">Can’t find what you’re looking for?</p>
+
+						<a href="#" class="inline-flex items-center mt-4 text-sm text-blue-500 gap-x-2 dark:text-blue-400 hover:underline">
+							<span>Chat to our team</span>
+
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 rtl:rotate-180">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+							</svg>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</body>
+
+</html>
